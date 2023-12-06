0b6f9676f3055399f3725f3daadd4a2483c3aff4
Ricardo <ricardo.remond.pro@gmail.com>
2023-12-06 14:10:11 +0100

    exo4 fini

---
+++ exo4.sql
@@ -0,0 +1,14 @@
+INSERT INTO `languages` (`id`, `language`, `version`)
+VALUES (NULL, 'javascript', '5'),
+    (NULL, 'PHP', '5.2'),
+    (NULL, ' PHP', '5.4'),
+    (NULL, 'HTML', '5.1'),
+    (NULL, 'JavaScript', '6'),
+    (NULL, 'JavaScript', '7'),
+    (NULL, 'JavaScript', '8'),
+    (NULL, 'PHP', '7');
+INSERT INTO `frameworks` (`id`, `framework`, `version`)
+VALUES (NULL, 'Symfony', '2.8'),
+    (NULL, 'Symfony', '1.3'),
+    (NULL, 'Jquery', '1.6'),
+    (NULL, 'Jquery', '2.10')
